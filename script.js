c4de17865e2fa73a2218fd0651f1094fd043d8dc
Pratik <erpratikrajverma@gmail.com>
2024-10-23 22:34:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// console.log('hello world')
+
+// console.log('code 1')
+
+// setTimeout(()=>{
+//     console.log('code 2')
+// },5000)
+
+// console.log('code 3')
+
+
+// const x = 10;
+
+// let x = 10;
+ 
+
+// {
+//     var x = 10;
+//     // let x = 10;
+//     //     x = 20;
+
+//     var x = 20;
+
+//     console.log(x)
+// }
+
+// {
+//     console.log(x)
+// }
+
+// let x = 10;
+
+// let y = '10'
+
+// console.log(x === y)
+
+
+
+//..................... function ...............................
+
+
+// function temp()
+// {
+//     console.log('hello world');
+// }
+
+// temp();
+
+// const temp = function(){
+//     console.log('hello world');
+// }
+
+
+// const temp = () => {
+//     console.log('hello world');
+// }
+
+
+
+
+// temp();
+
+
+// function parent(callback){
+//     console.log('this is parent')
+
+//     callback()
+// }
+
+
+
+
+// parent( () => {
+//     console.log('this is child')
+// })
+
+
